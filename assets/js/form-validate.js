306ee6d37e30ca802f0884d8bdcf804500c09ec5
tygfuidseujzx@gmail.com <tygfuidseujzx@gmail.com>
2025-06-04 10:44:41 +0800

    修改email-bug V-1.1.4

--- assets/js/form-validate.js
+++ assets/js/form-validate.js
@@ -1,19 +1,19 @@
 function valid_datas( f ){
 	
 	if( f.name.value == '' ){
-		jQuery('#form_status').html('<span class="wrong">Your name must not be empty!</span>');
+		jQuery('#form_status').html('<span class="wrong">你的email欄位不能是空的</span>');
 		notice( f.name );
 	}else if( f.email.value == '' ){
-		jQuery('#form_status').html('<span class="wrong">Your email must not be empty and correct format!</span>');
+		jQuery('#form_status').html('<span class="wrong">您的電子郵件不能是空且格式必須正確!</span>');
 		notice( f.email );
 	//}else if( f.phone.value == '' ){
 		//jQuery('#form_status').html('<span class="wrong">Your phone must not be empty and correct format!</span>');
 		//notice( f.phone );
 	}else if( f.subject.value == '' ){
-		jQuery('#form_status').html('<span class="wrong">Your subject must not be empty!</span>');
+		jQuery('#form_status').html('<span class="wrong">您的問題不能為空！</span>');
 		notice( f.subject );
 	}else if( f.message.value == '' ){
-		jQuery('#form_status').html('<span class="wrong">Your message must not be empty!</span>');
+		jQuery('#form_status').html('<span class="wrong">你必須描述你的問題詳情。</span>');
 		notice( f.message );
 	}else{
 		 jQuery.ajax({
@@ -27,7 +27,7 @@ function valid_datas( f ){
 				jQuery('#fruitkha-contact').remove();
 			}
 		});
-		jQuery('#form_status').html('<span class="loading">Sending your message...</span>');
+		jQuery('#form_status').html('<span class="loading">你的信件正在寄送...</span>');
 		jQuery('#fruitkha-contact').animate({opacity:0.3});
 		jQuery('#fruitkha-contact').find('input,textarea,button').css('border','none').attr({'disabled':''});
 	}
